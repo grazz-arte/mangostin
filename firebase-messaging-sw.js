99554521369a188dba2addb75112044cd5e24cec
Grazzi <gacascardi@outlook.com>
2026-06-26 21:26:53 -0300

    Add Firebase messaging service worker

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB98g-NFWwOS9_c1Ojr1rwnPtjkedI5bCg",
+    authDomain: "mangostin-notifications.firebaseapp.com",
+      projectId: "mangostin-notifications",
+        messagingSenderId: "542635383072",
+          appId: "1:542635383072:web:ef298991b3270a48301f45"
+          });
+
+          const messaging = firebase.messaging();
+
+          // 📲 recebe notificação mesmo fechado
+          messaging.onBackgroundMessage((payload) => {
+            self.registration.showNotification(payload.notification.title, {
+                body: payload.notification.body
+                  });
+                  });
