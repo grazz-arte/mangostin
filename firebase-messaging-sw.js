a4d6115d1ff39022b24ae1ebcf9b82870029fc75
Grazzi <gacascardi@outlook.com>
2026-07-07 19:00:13 -0300

    Atualizar o firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,17 +3,21 @@ importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 
 firebase.initializeApp({
   apiKey: "AIzaSyB98g-NFWwOS9_c1Ojr1rwnPtjkedI5bCg",
-    authDomain: "mangostin-notifications.firebaseapp.com",
-      projectId: "mangostin-notifications",
-        messagingSenderId: "542635383072",
-          appId: "1:542635383072:web:ef298991b3270a48301f45"
-          });
+  authDomain: "mangostin-notifications.firebaseapp.com",
+  projectId: "mangostin-notifications",
+  storageBucket: "mangostin-notifications.firebasestorage.app",
+  messagingSenderId: "542635383072",
+  appId: "1:542635383072:web:ef298991b3270a48301f45"
+});
 
-          const messaging = firebase.messaging();
+const messaging = firebase.messaging();
 
-          // 📲 recebe notificação mesmo fechado
-          messaging.onBackgroundMessage((payload) => {
-            self.registration.showNotification(payload.notification.title, {
-                body: payload.notification.body
-                  });
-                  });
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon-192.png"
+    }
+  );
+});
