e53e741d652cc5223bfc0e30e84a4a2d7a33c734
Grazzi <gacascardi@outlook.com>
2026-07-07 19:04:28 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 alert("APP INICIOU");
 
 const firebaseConfig = {
-    apiKey: "...",
-    authDomain: "...",
-    projectId: "...",
-    storageBucket: "...",
-    messagingSenderId: "...",
-    appId: "...",
-    measurementId: "..."
+  apiKey: "AIzaSyB98g-NFWwOS9_c1Ojr1rwnPtjkedI5bCg",
+  authDomain: "mangostin-notifications.firebaseapp.com",
+  projectId: "mangostin-notifications",
+  storageBucket: "mangostin-notifications.firebasestorage.app",
+  messagingSenderId: "542635383072",
+  appId: "1:542635383072:web:ef298991b3270a48301f45"
 };
-
 alert("ANTES DO INITIALIZE");
 
 firebase.initializeApp(firebaseConfig);
